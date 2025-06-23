08df04ecb3f26df99fd1cb6616580c3574d57a91
M4nu3lgit56 <manuelhmhd@gmail.com>
2025-06-23 13:56:41 -0500

    probar ingresar nombres

---
+++ script.js
@@ -0,0 +1,24 @@
+import "inc/database.js";
+
+const names = [];
+const name = document.getElementById("name");
+const addNameButton = document.getElementById("addName");
+const nameList = document.getElementById("nameList");
+
+addNameButton.addEventListener("click", async () => {
+    names.push(name.value);
+    name.value = "";
+    const { data, error } = await supabase
+        .from('names')
+        .insert([{ name: names[names.length - 1] }]);
+    if (error) {
+        console.error("Error inserting name:", error);
+    }
+
+    data.forEach(e => {
+        const li = document.createElement("li");
+        li.textContent = e.name;
+        nameList.appendChild(li);
+    });
+});
+
