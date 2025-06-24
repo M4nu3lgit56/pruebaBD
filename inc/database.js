61ae314b03f36a9d33f5133dd20d4f562eb56cd7
M4nu3lgit56 <manuelhmhd@gmail.com>
2025-06-23 20:23:05 -0500

    proteccion de db

--- inc/database.js
+++ inc/database.js
@@ -1,5 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = 'https://sqdjywwbdizqwozyaumf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxZGp5d3diZGl6cXdvenlhdW1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MzQ5NjgsImV4cCI6MjA2NjExMDk2OH0.i1tvFAztuyYJCdD2N1VxogG9hZZSq9t0pE1ybPlR3MQ';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
